19fd414dc8715ce92e205a36c6b6e3a218607014
Raoul Wadhwa <raoulwadhwa@gmail.com>
2025-01-17 10:50:58 -0600

    2015 day17

--- 2015/day17.cpp
+++ 2015/day17.cpp
@@ -9,6 +9,38 @@ vector<int> values;     // provided as input
 const int SUM = 150;
 int **subsets;          // fill in w/ DP processing
 
+bool *included;
+int comboCounter;
+const int FINAL_DEPTH = 4; // simple manual calculation for part 2
+
+// search for all combos of size FINAL_DEPTH that add up to SUM
+void dfsCombo(int depth, int lastIndex)
+{
+    // leaf node: check if combo adds up to SUM
+    if (depth == 4)
+    {
+        int counter = 0;
+        for (int i = 0; i < values.size(); i++)
+            if (included[i])
+                counter += values[i];
+
+        if (counter == SUM)
+            comboCounter++;
+
+        return ;
+    }
+
+    // try all the remaining combos
+    for (int i = lastIndex + 1; i < values.size(); i++)
+    {
+        if (included[i]) continue;
+
+        included[i] = true;
+        dfsCombo(depth + 1, i);
+        included[i] = false;
+    }
+}
+
 int main()
 {
     // process input
@@ -52,10 +84,20 @@ int main()
     // output: part 1
     cout << "Part 1: " << subsets[values.size()][SUM] << endl;
 
+    // part 2: took a couple min to manually figure out 4 are needed
+    // try all combinations of 4 and count how many add up to 150
+    included = new bool[values.size()];
+    comboCounter = 0;
+    dfsCombo(0, -1);
+
+    // output: part 2
+    cout << "Part 2: " << comboCounter << endl;
+
     // close out and exit
     for (int i = 0; i <= values.size(); i++)
         delete[] subsets[i];
     delete[] subsets;
+    delete[] included;
     values.clear();
     return 0;
 }
